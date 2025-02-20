dfbff8f0485921d2a024dac881c8b018dff8bfe6
dotfury <cm2jr4@gmail.com>
2025-02-20 13:00:14 +0900

    continue store setup - 2025/02/20

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css';
 import Store from '@/services/store';
 
+// find out more URL: https://www.google.com/search?q={fruit}
+
 window.addEventListener('DOMContentLoaded', async () => {
   Store.init();
   console.log(Store);
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,13 +1,59 @@
 import API from '@/services/api';
 
-const Store = {
+interface Fruit {
+  family: string;
+  genus: string;
+  id: number;
+  name: string;
+  order: string;
+  nutritions: {
+    [key: string]: number;
+  };
+}
+
+interface Store {
+  featuredFruit: Fruit | null;
+  featuredFruitIndex: number;
+  allFruit: Fruit[];
+  fruitCount: number;
+  suggestedFruit: Fruit[];
+  init: () => void;
+  setFeaturedFruit: () => void;
+  setSuggestedFruit: () => void;
+  getRandomFruit: () => Fruit;
+}
+
+const Store: Store = {
   featuredFruit: null,
-  featuredFruitIndex: null,
+  featuredFruitIndex: Infinity,
   allFruit: [],
+  fruitCount: Infinity,
   suggestedFruit: [],
   init: async () => {
     const fruits = await API.fetchFruit();
     Store.allFruit = fruits;
+    Store.fruitCount = Store.allFruit.length;
+    Store.setFeaturedFruit();
+    Store.setSuggestedFruit();
+  },
+  setFeaturedFruit: () => {
+    Store.featuredFruitIndex = Math.round(
+      Math.random() * (Store.fruitCount - 1)
+    );
+    Store.featuredFruit = Store.allFruit[Store.featuredFruitIndex];
+  },
+  setSuggestedFruit: () => {
+    const result: Fruit[] = new Array(5).fill(1);
+
+    Store.suggestedFruit = result.map(() => Store.getRandomFruit());
+  },
+  getRandomFruit: (): Fruit => {
+    // TODO: dont repeat suggested fruit
+    const index = Math.round(Math.random() * (Store.fruitCount - 1));
+
+    return index !== Store.featuredFruitIndex
+      ? Store.allFruit[index]
+      : Store.getRandomFruit();
   },
 };
 
