380cd7f8d6fcceb344818555a2fbd2a6020f7dfa
dotfury <cm2jr4@gmail.com>
2025-03-17 12:57:02 +0900

    update detail template + update recommended fruits styles - 2025/03/17

--- src/components/FruitDetail.ts
+++ src/components/FruitDetail.ts
@@ -18,15 +18,26 @@ export default class FruitDetail extends HTMLElement {
   render() {
     const fruit = getFruit(this.dataset.fruitId);
     const head = this.querySelector('h3')!;
-    const text = this.querySelector('p')!;
+    const family = this.querySelector('.family')!;
+    const nutrition = this.querySelector('.nutrition')!;
     const link = this.querySelector('a')!;
 
     if (!fruit) {
       head.textContent = 'Not Found';
-      text.textContent = `No data on ${this.dataset.fruitId} was found.`;
+      nutrition.textContent = `No data on ${this.dataset.fruitId} was found.`;
     } else {
       head.textContent = fruit.name;
-      text.textContent = `${fruit.nutritions.calories}cal`;
+      family.textContent = `
+        family: ${fruit.family}
+        genus: ${fruit.genus}
+      `;
+      nutrition.textContent = `
+        calories: ${fruit.nutritions.calories}
+        protein: ${fruit.nutritions.protein}
+        carbohydrates: ${fruit.nutritions.carbohydrates}
+        fat: ${fruit.nutritions.fat}
+        sugar: ${fruit.nutritions.sugar}
+      `;
       link.textContent = `Find out more about ${fruit.name}`;
       link.setAttribute(
         'href',
